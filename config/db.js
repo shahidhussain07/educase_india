648ce78a864e57c550a8ed36e9c231c014090cd1
shahidhussain07 <sh604780@gmail>
2025-03-09 12:03:10 +0530

    db config

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(process.env.DB_URL, {
 	dialectOptions: {
 		ssl: {
 			require: true,
-			rejectUnauthorized: false, // For PlanetScale SSL
+			rejectUnauthorized: false,
 		},
 	},
 })
